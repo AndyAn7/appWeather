81cd12d3e511758dc1bf6cbb3541ced8987b23c1
Andrew An <andyan7@gmail.com>
2022-01-30 22:26:07 -0800

    Added moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// global variables
 var form = document.querySelector('#form');
 var cityInterest = document.querySelector('#city');
 var button = document.querySelector('#btn-div')
@@ -12,3 +13,7 @@ let previousSearches = JSON.parse(localStorage.getItem('recent')) || [];
 var deletePrev = document.querySelector('#clear-btn');
 var container = document.querySelector('#card-container');
 /* https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=5490ec2aa2dd516ff7e09f6aff3cf7c0 */
+
+// current moment
+var now = moment();
+currentDay.textContent = (now.format("dddd, MMMM Do YYYY"));
